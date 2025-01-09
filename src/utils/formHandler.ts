0727350e9a517a332de026ffc81fb905428c7a02
Graham Van Pelt <grahamvanpelt@gmail.com>
2025-01-09 09:30:48 -0800

    feat: refactor form handler

---
+++ src/utils/formHandler.ts
@@ -0,0 +1,79 @@
+import { db, ToDos, Categories, eq } from "astro:db"
+import { getFormDataValue, calculateNewStreak } from "../utils/formUtils"
+
+export async function handleFormSubmission(formData: FormData) {
+  const formType = formData.get("formType")
+
+  switch (formType) {
+    case "todo": {
+      const categoryId = formData.get("categoryId")
+      const name = formData.get("name")
+      if (typeof categoryId === "string" && typeof name === "string") {
+        await db.insert(ToDos).values({
+          categoryId,
+          name,
+          checked: false,
+          streak: 0,
+          id: crypto.randomUUID(),
+        })
+      }
+      break
+    }
+
+    case "category": {
+      const name = formData.get("name")
+      if (typeof name === "string") {
+        await db.insert(Categories).values({
+          name,
+          id: crypto.randomUUID(),
+        })
+      }
+      break
+    }
+
+    case "removeCategory": {
+      const categoryId = getFormDataValue(formData, "categoryId")
+
+      const todosInCategory = await db
+        .select()
+        .from(ToDos)
+        .where(eq(ToDos.categoryId, categoryId as string))
+
+      if (todosInCategory.length > 0) {
+        try {
+          for (const todo of todosInCategory) {
+            await db.delete(ToDos).where(eq(ToDos.id, todo.id))
+          }
+        } catch (error) {
+          console.log(error)
+        }
+      }
+
+      if (categoryId) {
+        await db.delete(Categories).where(eq(Categories.id, categoryId))
+      }
+      break
+    }
+
+    case "updateTodo": {
+      const todoId = getFormDataValue(formData, "todoId")
+      const streakValue = getFormDataValue(formData, "streakValue")
+      const isChecked = formData.get("checked") === "on"
+      if (todoId) {
+        const newStreakValue = calculateNewStreak(streakValue, isChecked)
+        await db
+          .update(ToDos)
+          .set({
+            checked: isChecked,
+            streak: newStreakValue,
+          })
+          .where(eq(ToDos.id, todoId))
+      }
+      break
+    }
+
+    default:
+      console.log(`Unknown form type: ${formType}`)
+      break
+  }
+}

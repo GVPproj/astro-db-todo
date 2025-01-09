8b7f9336a42d69719017f93385597644224c9d4f
Graham Van Pelt <grahamvanpelt@gmail.com>
2025-01-09 14:34:20 -0800

    feat: add github auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import db from '@astrojs/db';
+import netlify from '@astrojs/netlify'
+import auth from 'auth-astro';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db()],
-  output: "server"
+  integrations: [db(), auth()],
+  output: "server",
+  adapter: netlify(),
 });
